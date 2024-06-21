1e4b37db8622cc67f4bb201a25d7ff5578d9deda
Pham-Xuan-Son <phamxuansons@gmailcom>
2024-06-21 20:07:01 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ let x = new BigNumber("11111111111111");
 let y = x.multipliedBy("5723507230632470698734963476");
 let z = y.dividedBy("5723507230632470698734963476");
 
-("1.22 / 0.3 =");
+// ("1.22 / 0.3 =");
 
 let first = {
   numerator: 122,
